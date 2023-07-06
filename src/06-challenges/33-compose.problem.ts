6481662c029300daab1ba421f8b6a3bae0ee2f8f
Kevin Agus <kevin.agus@alten.it>
2023-07-06 11:01:15 +0200

    fix: solution 33

--- src/06-challenges/33-compose.problem.ts
+++ src/06-challenges/33-compose.problem.ts
@@ -1,11 +1,21 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-export const compose =
-  (...funcs: Array<(input: any) => any>) =>
-  (input: any) => {
+function compose<T1, T2>(func: (input: T1) => T2): (input: T1) => T2;
+function compose<T1, T2, T3>(
+  func1: (input: T1) => T2,
+  func2: (input: T2) => T3
+): (input: T1) => T3;
+function compose<T1, T2, T3, T4>(
+  func1: (input: T1) => T2,
+  func2: (input: T2) => T3,
+  func3: (input: T3) => T4
+): (input: T1) => T4;
+function compose(...funcs: Array<(input: any) => any>) {
+  return (input: any) => {
     return funcs.reduce((acc, fn) => fn(acc), input);
   };
+}
 
 const addOne = (num: number) => {
   return num + 1;
@@ -27,6 +37,6 @@ it("Should error when the input to a function is not typed correctly", () => {
     // a function that returns a string!
     // @ts-expect-error
     String,
-    addOne,
+    addOne
   );
 });
